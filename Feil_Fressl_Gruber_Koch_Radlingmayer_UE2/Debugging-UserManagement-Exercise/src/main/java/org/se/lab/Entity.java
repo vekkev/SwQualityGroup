af13749f0f18cfe2931045b32971e4deef0a5dc5
MaxiFeil-MacBook <maximilian.feil@edu.fh-joanneum.at>
2018-11-06 12:47:52 +0100

    added Debuging-UserManagement with findbugs plugin installed

---
+++ Feil_Fressl_Gruber_Koch_Radlingmayer_UE2/Debugging-UserManagement-Exercise/src/main/java/org/se/lab/Entity.java
@@ -0,0 +1,17 @@
+package org.se.lab;
+
+public abstract class Entity
+{
+    /*
+     * Property: id:int
+     */
+    private int id;
+    public int getId()
+    {
+        return id;
+    }
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+}
