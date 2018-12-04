e47ab2645eaaec5b8fac32ce6ee4e2ff70b9fcc3
Kevin Gruber <kevin-gruber1@gmx.at>
2018-12-04 21:13:17 +0100

    solved Stub ArticleServiceTestWithSpyAndStub

--- Radlingmayer_Fressl_Gruber_Koch_Feil_UE3/JUnit4-TestDouble-Spy-ArticleService-Exercise/src/test/java/org/se/lab/ArticleServiceTestWithSpyAndStub.java
+++ Radlingmayer_Fressl_Gruber_Koch_Feil_UE3/JUnit4-TestDouble-Spy-ArticleService-Exercise/src/test/java/org/se/lab/ArticleServiceTestWithSpyAndStub.java
@@ -1,68 +1,71 @@
 package org.se.lab;
 
-import java.util.Arrays;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ArticleServiceTestWithSpyAndStub
-{
+import java.util.Arrays;
+
+public class ArticleServiceTestWithSpyAndStub {
 	private ArticleTableStub stub;
 	private ArticleTableSpy spy;
 	private ArticleService service;
-	
+
 	@Before
-	public void setup()
-	{
+	public void setup() {
 		// service -> spy -> stub
 		stub = new ArticleTableStub();
 		spy = new ArticleTableSpy(stub);
 		service = new ArticleService(spy);
 	}
-	
+
 	@Test
-	public void testAddArticle()
-	{
+	public void testAddArticle() {
 		// setup
 		Article book = new Article(1, "Effective Java", 3495);
-		
+
 		// exercise
 		service.addArticle(book);
-		
+
 		// state verification
 		Article a = stub.articles.get(0);
 		Assert.assertEquals(1, a.getId());
 		Assert.assertEquals("Effective Java", a.getDescription());
 		Assert.assertEquals(3495, a.getPrice());
-		
+
 		// behavioral verification
 		Assert.assertEquals(1, spy.logs.size());
 		Assert.assertTrue(spy.logs.get(0).contains("ArticleTable.insert"));
 
 		// TODO: validate parameter data
+
+		Assert.assertTrue(spy.logs.get(0).contains("1,Effective Java,3495"));
 	}
-	
+
+
 	@Test
-	public void testNumberOfArticles()
-	{
+	public void testNumberOfArticles() {
 		// setup
 		stub.articles = Arrays.asList(
-				new Article(1, "Effective Java", 3495), 
+				new Article(1, "Effective Java", 3495),
 				new Article(2, "Java Concurrency in Practice", 3895),
 				new Article(3, "Clean Code: A Handbook of Agile Software Craftsmanship", 3095)
-				);
-		 		
+		);
+
 		// exercise
 		int number = service.numberOfArticles();
-		
+
 		// state verification
-		Assert.assertEquals(3, number);		
+		Assert.assertEquals(3, number);
 
 		// behavioral verification
 		Assert.assertEquals(1, spy.logs.size());
 		Assert.assertTrue(spy.logs.get(0).contains("ArticleTable.findAll()"));
 
 		// TODO: Verify return data
-	}	
+		Assert.assertTrue(spy.logs.get(0).contains("1,Effective Java,3495"));
+		Assert.assertTrue(spy.logs.get(0).contains("2,Java Concurrency in Practice,3895"));
+		Assert.assertTrue(spy.logs.get(0).contains("3,Clean Code: A Handbook of Agile Software Craftsmanship,3095"));
+
+	}
 }
--- Radlingmayer_Fressl_Gruber_Koch_Feil_UE3/JUnit4-TestDouble-Spy-ArticleService-Exercise/src/test/java/org/se/lab/ArticleTableSpy.java
+++ Radlingmayer_Fressl_Gruber_Koch_Feil_UE3/JUnit4-TestDouble-Spy-ArticleService-Exercise/src/test/java/org/se/lab/ArticleTableSpy.java
@@ -1,4 +1,47 @@
 package org.se.lab;
 
-public class ArticleTableSpy {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArticleTableSpy implements ArticleTable {
+    public List<String> logs = new ArrayList<String>();
+    private ArticleTable articleTable;
+
+
+    @Override
+    public void insert(Article p) {
+        logs.add("ArticleTable.insert " + p);
+        articleTable.insert(p);
+    }
+
+    @Override
+    public void update(Article p) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public Article findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Article> findAll() {
+        List<Article> articles = articleTable.findAll();
+        String s = "ArticleTable.findAll() " + articles;
+        logs.add(s);
+        return articles;
+    }
+
+    public ArticleTableSpy(ArticleTable articleTable) {
+        if (articleTable == null) {
+            throw new IllegalArgumentException();
+        }
+        this.articleTable = articleTable;
+    }
+
 }
