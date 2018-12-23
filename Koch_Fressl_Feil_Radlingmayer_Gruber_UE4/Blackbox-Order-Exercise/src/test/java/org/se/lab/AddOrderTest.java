a5e8e9318468a9dd837d8e033136533d1bc01c2c
Hotzenplotz12 <christian.radlingmayer@edu.fh-joanneum.at>
2018-12-23 18:30:58 +0100

    TestCases added

--- Koch_Fressl_Feil_Radlingmayer_Gruber_UE4/Blackbox-Order-Exercise/src/test/java/org/se/lab/AddOrderTest.java
+++ Koch_Fressl_Feil_Radlingmayer_Gruber_UE4/Blackbox-Order-Exercise/src/test/java/org/se/lab/AddOrderTest.java
@@ -1,6 +1,11 @@
 package org.se.lab;
 
 import org.junit.Before;
+import org.junit.*;
+
+import java.time.temporal.Temporal;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddOrderTest
 {
@@ -17,10 +22,78 @@ public class AddOrderTest
 	
 	// Version 1: use a single test method for each test case
 
-	// TODO
+	@Test
+	public void testValidArticleMin()
+	{
+		order.addOrder(1,0);
+	}
+
+	@Test
+	public void testValidArticleMax()
+	{
+		order.addOrder(9,10000);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testTooSmallArticleNumber()
+	{
+		order.addOrder(1,-1);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testTooBigArticleNumber()
+	{
+		order.addOrder(1,1000022);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testTooSmallQuantity()
+	{
+		order.addOrder(0,99);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testTooBigQuantity()
+	{
+		order.addOrder(11,99);
+	}
 
 	
 	// Version 2: use a map of test cases
-	
-	// TODO
+
+
+	@Test
+	public void testMap()
+	{
+		HashMap<TestParameters, IllegalArgumentException> fullTestCases = new HashMap<>();
+
+		fullTestCases.put(new TestParameters(1,0), null);
+		fullTestCases.put(new TestParameters(9, 10000), null);
+		fullTestCases.put(new TestParameters(1,-1), new IllegalArgumentException("Wrong articleNr: -1"));
+		fullTestCases.put(new TestParameters(1, 1000022), new IllegalArgumentException("Wrong articleNr: 1000022"));
+		fullTestCases.put(new TestParameters(0,99), new IllegalArgumentException("Wrong quantity: 0"));
+		fullTestCases.put(new TestParameters(11,99), new IllegalArgumentException("Wrong quantity: 11"));
+
+		runTestCases(fullTestCases);
+	}
+
+	public void runTestCases(Map<TestParameters, IllegalArgumentException> testCases)
+	{
+		for( TestParameters params : testCases.keySet())
+		{
+			try
+			{
+				order.addOrder(params.getQuantity(),params.getArticleNr());
+				if(testCases.get(params) != null)
+				{
+					Assert.fail();
+				}
+			}
+			catch (IllegalArgumentException e)
+			{
+				Assert.assertEquals(testCases.get(params).getMessage(), e.getMessage());
+			}
+		}
+	}
+
 }
